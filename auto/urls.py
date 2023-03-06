b02e863beb3cdf55e1b3c3119da83b7edb83e311
KurochkaR <kurochkar001@gmail.com>
2023-03-06 20:40:04 +0200

    try clean history

--- auto/urls.py
+++ auto/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     path('cars/', gps_cars, name='map'),
     path('', include('taxi_service.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
