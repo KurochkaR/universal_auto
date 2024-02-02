9bd01563a117a1ac9dd48f8448281d4db0dedfb2
OlegBondarUA <Petrovichteh@gmail.com>
2024-02-02 13:16:21 +0200

    upd style, script payments

--- static/dashboard/js/scripts-payments.js
+++ static/dashboard/js/scripts-payments.js
@@ -29,7 +29,7 @@ function driverPayment(period = null, start = null, end = null, paymentStatus = 
 					var payByn = '<button class="pay-btn">Отримано</button>';
 					var notPayByn = '<button class="not-pay-btn">Не отримано</button>';
 
-					var rowBonus = '<tr><td colspan="11" class="bonus-table"><table class="bonus-penalty-table"><tr class="title-bonus-penalty"><th class="edit-bonus-penalty">Тип</th><th class="edit-bonus-penalty">Сума</th><th class="edit-bonus-penalty">Опис</th>' + (response[i].status === 'Перевіряється' ? '<th class="edit-bonus-penalty">Дії</th>' : '') + '</tr>';
+					var rowBonus = '<tr class="tr-driver-payments"><td colspan="11" class="bonus-table"><table class="bonus-penalty-table"><tr class="title-bonus-penalty"><th class="edit-bonus-penalty">Тип</th><th class="edit-bonus-penalty">Сума</th><th class="edit-bonus-penalty">Опис</th>' + (response[i].status === 'Перевіряється' ? '<th class="edit-bonus-penalty">Дії</th>' : '') + '</tr>';
 
 					function generateRow(items, type, editClass, deleteClass) {
 						var rowBon = '';
@@ -50,18 +50,26 @@ function driverPayment(period = null, start = null, end = null, paymentStatus = 
 					rowBonus += generateRow(response[i].bonuses_list, 'bonus', 'edit-bonus-btn', 'delete-bonus-btn');
 					rowBonus += generateRow(response[i].penalties_list, 'penalty', 'edit-penalty-btn', 'delete-penalty-btn');
 					rowBonus += '</table></td></tr>';
-
-					var row = $('<tr>');
+					var salary = response[i].salary;
+					if (response[i].salary <= 0) {
+						salary = "0.00";
+					}
+					var row = $('<tr class="tr-driver-payments">');
 					row.attr('data-id', response[i].id);
 					row.append('<td>' + response[i].report_from + ' - ' + response[i].report_to + '</td>');
 					row.append('<td class="driver-name cell-with-triangle" title="Розкрити для розширеного огляду Бонусів та штрафів">' + response[i].full_name + ' <i class="fa fa-caret-down"></i></td>');
 					row.append('<td>' + response[i].kasa + '</td>');
 					row.append('<td>' + response[i].cash + '</td>');
 					row.append('<td>' + response[i].rent + '</td>');
-					row.append('<td>' + '<div style="display: flex;justify-content: space-evenly; align-items: center;">' + response[i].bonuses  + addButtonBonus + '</div>' + '</td>');
-					row.append('<td>' + '<div style="display: flex;justify-content: space-evenly; align-items: center;">' + response[i].penalties + addButtonPenalty + '</div>' + '</td>');
+					if (response[i].status === 'Перевіряється') {
+						row.append('<td>' + '<div style="display: flex;justify-content: space-evenly; align-items: center;">' + response[i].bonuses  + addButtonBonus + '</div>' + '</td>');
+						row.append('<td>' + '<div style="display: flex;justify-content: space-evenly; align-items: center;">' + response[i].penalties + addButtonPenalty + '</div>' + '</td>');
+					} else {
+						row.append('<td>' + '<div style="display: flex;justify-content: space-evenly; align-items: center;">' + response[i].bonuses + '</div>' + '</td>');
+					row.append('<td>' + '<div style="display: flex;justify-content: space-evenly; align-items: center;">' + response[i].penalties + '</div>' + '</td>');
+					}
 					row.append('<td>' + response[i].earning + '</td>');
-					row.append('<td>' + response[i].salary + '</td>');
+					row.append('<td>' + salary + '</td>');
 					row.append('<td>' + response[i].status + '</td>');
 					var showAllButton = $('.send-all-button');
 					showAllButton.hide(0);
